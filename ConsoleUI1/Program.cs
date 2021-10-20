15df8c0dc97d39e092a38831470b7e7f145dd3d3
buraksafak <buraksafak2109@gmail.com>
2021-10-20 14:56:08 +0200

    Global bir OOP yapisi olusturuldu

--- ConsoleUI1/Program.cs
+++ ConsoleUI1/Program.cs
@@ -9,13 +9,30 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            // ProductTest();
+
+            CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
             foreach (var item in productManager.GetAllByCategoryId(2))
             {
                 Console.WriteLine(item.ProductName);
             }
-           
         }
     }
 }
--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,4 +1,4 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,13 +6,13 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //where sozcugu ile kisitlama yapiyoruz. 
     //class = referans tip olabilir demek.
     //T, ya IEntity ya da IEntity'den implemente edilmis bir nesne olabilir.
-    //new'lenebilir olmali.
-    //tum bunlari, sistemimizin yalnizca veritabani nesneleri ile calisan bir repository olmasi icin yaptik.
+    //new' lenebilir olmali.
+    //tum bunlari, sistemimizin yalnizca veritabani nesneleri ile calisan bir repository olmasi icin gerceklestirdik.
     public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         //Expression, dinamik filtreleme yapmamıza yarıyor.(Delegate) 
