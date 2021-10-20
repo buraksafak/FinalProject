1d5bbb01fd02899b0c556c2a1c86955615171307
buraksafak <buraksafak2109@gmail.com>
2021-10-20 17:48:39 +0200

    ConsoleUI refactor edilldi

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,37 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // CategoryTest();
+
+            ProductTest();
+
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + " / " + product.ProductName);
+            }
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+    }
+}
+
